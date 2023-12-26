ac704b3a4807c455a74d674ecf6fb35fdf863edd
ernesto <ernestoponce27@gmail.com>
2023-12-26 02:24:31 -0300

    WIP - compact file

--- keyvaluedb/engine.go
+++ keyvaluedb/engine.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Engine struct {
 	m    map[string]int64
 	file *os.File
+	mu   sync.Mutex
 }
 
 func NewEngine(filename string) *Engine {
@@ -22,11 +27,14 @@ func NewEngine(filename string) *Engine {
 	return &Engine{
 		m:    make(map[string]int64),
 		file: file,
+		mu:   sync.Mutex{},
 	}
 }
 
 func (c *Engine) Get(key string) []byte {
-	// TODO VALIDATE KEY EXISTS
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if _, ok := c.m[key]; !ok {
 		return []byte{}
 	}
@@ -75,6 +83,64 @@ func (c *Engine) Set(key string, value string) {
 	}
 
 	c.m[key] = offset
+}
+
+func (c *Engine) CompactFile() {
+	for {
+		time.Sleep(5 * time.Second)
+		fmt.Println("Compacting file...")
+		c.mu.Lock()
+
+		backupFile, err := os.OpenFile("backup.txt", os.O_RDWR|os.O_TRUNC, 0644)
+		if err != nil {
+			fmt.Println("Error creating backup file:", err)
+			c.mu.Unlock()
+			continue
+		}
+
+		fmt.Println(c.file)
+		_, err = io.Copy(backupFile, c.file)
+		if err != nil {
+			fmt.Println("Error copying file contents to backup file:", err)
+			c.mu.Unlock()
+			backupFile.Close()
+			continue
+		}
+
+		_, err = c.file.Seek(0, 0)
+		if err != nil {
+			fmt.Println(err)
+			c.mu.Unlock()
+			continue
+		}
+
+		scanner := bufio.NewScanner(c.file)
+
+		m := make(map[string]string)
+		for scanner.Scan() {
+			line := scanner.Text()
+			parts := strings.Split(line, ":")
+			if len(parts) >= 2 {
+				m[parts[0]] = parts[1]
+			}
+		}
+
+		err = c.file.Truncate(0)
+		if err != nil {
+			fmt.Println(err)
+			c.mu.Unlock()
+			continue
+		}
+
+		for k, v := range m {
+			c.Set(k, v)
+		}
+
+		c.file.Seek(0, 0)
+		c.mu.Unlock()
+		backupFile.Close()
+
+	}
 }
 
 func (c *Engine) Close() {
--- keyvaluedb/main.go
+++ keyvaluedb/main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
 	// remove file
-	os.Remove("file.txt")
+	//os.Remove("file.txt")
 	cache := NewEngine("file.txt")
 	defer cache.Close()
-	cache.Set("key1", "value1")
-	cache.Set("key2", "value2")
-	r := cache.Get("key1")
-	fmt.Println(r)
+	// cache.Set("key4", "value1")
+	// cache.Set("key6", "value2")
+	// r := cache.Get("key1")
+	// fmt.Println(r)
+
+	go cache.CompactFile()
+
+	exitSignal := make(chan struct{})
+	<-exitSignal
 
 }
