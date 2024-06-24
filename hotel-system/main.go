69f26293e6b6631565edb90980e17f32027a29a5
ernesto <ernestoponce27@gmail.com>
2024-06-23 22:38:12 -0300

    update

--- hotel-system/main.go
+++ hotel-system/main.go
@@ -6,4 +6,12 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	// endpoint to make reservation
+
+	// send csrf token to client
+
+	// validate csrf token
+
+	// validate reservation body json
+
+	// vakudate if availability date hotel is available
 }
