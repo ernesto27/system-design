bfc6d075b1f0073d4d16afa8b78f907f90c5fb53
ernesto <ernestoponce27@gmail.com>
2024-11-03 18:57:26 -0300

    update

--- game-leader-board/dynamodb.go
+++ game-leader-board/dynamodb.go
@@ -15,24 +15,28 @@ import (
 )
 
 type DynamoDB struct {
-	client *dynamodb.Client
+	client    *dynamodb.Client
+	tableName string
 }
 
 type ScoreRequest struct {
-	UserID       int    `json:"user_id"`
-	GameID       string `json:"game_id"`
-	CurrentScore string `json:"current_score"`
-	Name         string `json:"name"`
+	UserID int    `json:"user_id"`
+	GameID string `json:"game_id"`
+	Name   string `json:"name"`
 }
 
 type LeaderBoard struct {
-	LeaderboardName string `json:"leaderboard_name"`
-	ScoreSort       string `json:"-"`
-	Score           string `json:"score"`
-	Name            string `json:"name"`
+	GameID string `json:"game_id"`
+	UserID int    `json:"user_id"`
+	Score  int    `json:"score" `
+	Rank   int    `json:"rank"`
 }
 
-func NewDynamoDB(key string, secret string, region string) *DynamoDB {
+type LeaderBoardByUserID struct {
+	LeaderBoard
+}
+
+func NewDynamoDB(key string, secret string, region string, tableName string) *DynamoDB {
 	creds := credentials.NewStaticCredentialsProvider(key, secret, "")
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
@@ -45,77 +49,63 @@ func NewDynamoDB(key string, secret string, region string) *DynamoDB {
 
 	ddbClient = dynamodb.NewFromConfig(cfg)
 	return &DynamoDB{
-		client: ddbClient,
+		client:    ddbClient,
+		tableName: tableName,
 	}
 }
 
 func (d *DynamoDB) PutItem(scoreRequest ScoreRequest) error {
 	input := &dynamodb.GetItemInput{
-		TableName: aws.String("leaderboard"),
+		TableName: aws.String(d.tableName),
 		Key: map[string]types.AttributeValue{
-			"leaderboard_name": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
-			"score":            &types.AttributeValueMemberS{Value: scoreRequest.CurrentScore},
+			"game_id": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
+			"user_id": &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", scoreRequest.UserID)},
 		},
 	}
 
-	result, err := myDB.client.GetItem(context.TODO(), input)
+	result, err := d.client.GetItem(context.TODO(), input)
 	if err != nil {
 		return err
 	}
 
-	if result.Item != nil {
-		fmt.Println("delete current item")
-		input := &dynamodb.DeleteItemInput{
-			TableName: aws.String("leaderboard"),
-			Key: map[string]types.AttributeValue{
-				"leaderboard_name": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
-				"score":            &types.AttributeValueMemberS{Value: scoreRequest.CurrentScore},
-			},
-		}
-
-		_, err := ddbClient.DeleteItem(context.TODO(), input)
-
-		if err != nil {
-			return err
-		}
-	}
-
-	var currScore int
-	if scoreRequest.CurrentScore == "666" {
-		currScore = 1
+	var scoreValue int
+	if result.Item == nil {
+		scoreValue = 1
 	} else {
-		currScore, err = parseScoreFromString(scoreRequest.CurrentScore)
+		scoreValue, err = strconv.Atoi(result.Item["score"].(*types.AttributeValueMemberN).Value)
 		if err != nil {
 			return err
 		}
-		currScore++
+		scoreValue += 1
 	}
-
-	newScore := padScore(currScore, scoreRequest.UserID)
 
 	putItemInput := &dynamodb.PutItemInput{
-		TableName: aws.String("leaderboard"),
+		TableName: aws.String(d.tableName),
 		Item: map[string]types.AttributeValue{
-			"leaderboard_name": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
-			"score":            &types.AttributeValueMemberS{Value: newScore},
-			"score_value":      &types.AttributeValueMemberN{Value: strconv.Itoa(currScore)},
-			"name":             &types.AttributeValueMemberS{Value: scoreRequest.Name},
+			"game_id": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
+			"user_id": &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", scoreRequest.UserID)},
+			"score":   &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", scoreValue)},
 		},
 	}
 
 	_, err = d.client.PutItem(context.TODO(), putItemInput)
+	if err != nil {
+		return err
+	}
 
-	return err
+	return nil
 }
 
-func (d *DynamoDB) GetLeaderboard(leaderboardName string) ([]LeaderBoard, error) {
+func (d *DynamoDB) GetLeaderboard(gameID string) ([]LeaderBoard, error) {
 	input := &dynamodb.QueryInput{
-		TableName:              aws.String("leaderboard"),
-		KeyConditionExpression: aws.String("leaderboard_name = :ln"),
+		TableName:              aws.String(d.tableName),
+		IndexName:              aws.String("game_id-score-index"),
+		KeyConditionExpression: aws.String("game_id = :ln"),
 		ExpressionAttributeValues: map[string]types.AttributeValue{
-			":ln": &types.AttributeValueMemberS{Value: leaderboardName},
+			":ln": &types.AttributeValueMemberS{Value: gameID},
 		},
 		ScanIndexForward: aws.Bool(false),
+		Limit:            aws.Int32(10),
 	}
 
 	result, err := d.client.Query(context.TODO(), input)
@@ -124,16 +114,137 @@ func (d *DynamoDB) GetLeaderboard(leaderboardName string) ([]LeaderBoard, error)
 	}
 
 	var leaderBoard []LeaderBoard
+	rank := 1
 	for _, item := range result.Items {
+		scoreValue, err := strconv.Atoi(item["score"].(*types.AttributeValueMemberN).Value)
+		if err != nil {
+			return nil, err
+		}
+		userIDValue, err := strconv.Atoi(item["user_id"].(*types.AttributeValueMemberN).Value)
+		if err != nil {
+			return nil, err
+		}
+
 		leaderBoard = append(leaderBoard, LeaderBoard{
-			LeaderboardName: item["leaderboard_name"].(*types.AttributeValueMemberS).Value,
-			Score:           item["score_value"].(*types.AttributeValueMemberN).Value,
-			Name:            item["name"].(*types.AttributeValueMemberS).Value,
+			GameID: item["game_id"].(*types.AttributeValueMemberS).Value,
+			UserID: userIDValue,
+			Score:  scoreValue,
+			Rank:   rank,
 		})
 
+		rank++
 	}
 
 	return leaderBoard, nil
+}
+
+func (d *DynamoDB) GetLeaderBoardByUserID(scoreRequest ScoreRequest) (LeaderBoardByUserID, error) {
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String(d.tableName),
+		Key: map[string]types.AttributeValue{
+			"game_id": &types.AttributeValueMemberS{Value: scoreRequest.GameID},
+			"user_id": &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", scoreRequest.UserID)},
+		},
+	}
+
+	var leaderBoard LeaderBoardByUserID
+	result, err := d.client.GetItem(context.TODO(), input)
+	if err != nil {
+		return leaderBoard, err
+	}
+
+	if result.Item == nil {
+		return leaderBoard, fmt.Errorf("user_id not found")
+	}
+
+	scoreValue, err := strconv.Atoi(result.Item["score"].(*types.AttributeValueMemberN).Value)
+	if err != nil {
+		return leaderBoard, err
+	}
+
+	gameId := scoreRequest.GameID
+	userScore := scoreValue
+
+	queryInput := &dynamodb.QueryInput{
+		TableName:              aws.String(d.tableName),
+		IndexName:              aws.String("game_id-score-index"),
+		KeyConditionExpression: aws.String("game_id = :game_id AND score > :score"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":game_id": &types.AttributeValueMemberS{Value: gameId},
+			":score":   &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", userScore)},
+		},
+		Select: types.SelectAllAttributes,
+	}
+
+	queryResult, err := d.client.Query(context.TODO(), queryInput)
+	if err != nil {
+		return leaderBoard, err
+	}
+
+	fmt.Println(queryResult.Items)
+
+	leaderBoard.GameID = scoreRequest.GameID
+	leaderBoard.UserID = scoreRequest.UserID
+	leaderBoard.Score = scoreValue
+	leaderBoard.Rank = int(queryResult.Count) + 1
+
+	return leaderBoard, nil
+}
+
+func (d *DynamoDB) Test() {
+	// Get ranking
+	// input := &dynamodb.QueryInput{
+	// 	TableName:              aws.String("test_leaderboar"),
+	// 	IndexName:              aws.String("game_id-score-index"),
+	// 	KeyConditionExpression: aws.String("game_id = :ln"),
+	// 	ExpressionAttributeValues: map[string]types.AttributeValue{
+	// 		":ln": &types.AttributeValueMemberS{Value: "game1"},
+	// 	},
+	// 	ScanIndexForward: aws.Bool(true),
+	// }
+
+	// result, err := d.client.Query(context.TODO(), input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, item := range result.Items {
+	// 	fmt.Println(item["score"].(*types.AttributeValueMemberN).Value)
+	// }
+
+	// Get by user by id
+	// input := &dynamodb.GetItemInput{
+	// 	TableName: aws.String("test_leaderboar"),
+	// 	Key: map[string]types.AttributeValue{
+	// 		"game_id": &types.AttributeValueMemberS{Value: "game1"},
+	// 		"user_id": &types.AttributeValueMemberN{Value: "12"},
+	// 	},
+	// }
+
+	// result, err := d.client.GetItem(context.TODO(), input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(result.Item["user_id"].(*types.AttributeValueMemberN).Value)
+
+	// Put item
+	putItemInput := &dynamodb.PutItemInput{
+		TableName: aws.String("test_leaderboar"),
+		Item: map[string]types.AttributeValue{
+			"game_id": &types.AttributeValueMemberS{Value: "game1"},
+			"user_id": &types.AttributeValueMemberN{Value: "100"},
+			"score":   &types.AttributeValueMemberN{Value: "66"},
+		},
+	}
+
+	_, err := d.client.PutItem(context.TODO(), putItemInput)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Put item success")
+
 }
 
 func padScore(score int, userID int) string {
--- game-leader-board/main.go
+++ game-leader-board/main.go
@@ -28,7 +28,7 @@ func handleAddUpdateScore(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if entry.UserID == 0 || entry.GameID == "" || entry.CurrentScore == "" {
+	if entry.UserID == 0 || entry.GameID == "" {
 		fmt.Println("Invalid request body")
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
@@ -47,13 +47,13 @@ func handleAddUpdateScore(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGetLeaderboard(w http.ResponseWriter, r *http.Request) {
-	leaderboardName := r.URL.Query().Get("leaderboard_name")
-	if leaderboardName == "" {
+	gameID := r.URL.Query().Get("game_id")
+	if gameID == "" {
 		http.Error(w, "leaderboard_name is required", http.StatusBadRequest)
 		return
 	}
 
-	entries, err := myDB.GetLeaderboard(leaderboardName)
+	entries, err := myDB.GetLeaderboard(gameID)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Failed to get leaderboard", http.StatusInternalServerError)
@@ -64,6 +64,33 @@ func handleGetLeaderboard(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(entries)
 }
 
+func handleGetScoreByUserID(w http.ResponseWriter, r *http.Request) {
+	var scoreRequest ScoreRequest
+	err := json.NewDecoder(r.Body).Decode(&scoreRequest)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+		return
+	}
+
+	if scoreRequest.UserID == 0 || scoreRequest.GameID == "" {
+		fmt.Println("Invalid request body")
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	leaderBoard, err := myDB.GetLeaderBoardByUserID(scoreRequest)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Failed to get score", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(leaderBoard)
+
+}
+
 var myDB *DynamoDB
 
 func main() {
@@ -71,12 +98,17 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	myDB = NewDynamoDB(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_REGION"))
+	myDB = NewDynamoDB(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		os.Getenv("AWS_REGION"), "test_leaderboar")
+
+	// myDB.Test()
+	// os.Exit(1)
 
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /score", handleAddUpdateScore)
 	router.HandleFunc("GET /leaderboard", handleGetLeaderboard)
+	router.HandleFunc("GET /score/user", handleGetScoreByUserID)
 
 	server := http.Server{
 		Addr:    ":8080",
